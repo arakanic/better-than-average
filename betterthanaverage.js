092c75e5a7bbae5dcd7aca3d1d336e26282324d2
Ara Kanic <ji.arakani@gmail.com>
2022-05-18 20:35:51 -0400

    Update betterthanaverage.js

--- betterthanaverage.js
+++ betterthanaverage.js
@@ -1,4 +1,4 @@
-// This function indicates whether you scored higher than your class average, given everybody's score in an array of integer and yours as an integer!
+// Given everybody else's scores (in an array) and your score (as integers), this function indicates whether you scored higher than your class average!
 function betterThanAverage(classPoints, yourPoints) {
     const classAverage = classPoints.reduce((sum, n) => sum + n, 0) / classPoints.length
     return yourPoints > classAverage
